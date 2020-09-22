b01cf1c6120bcb8316232740dd04c6aa3ba0547b
felixzhu00 <felix.zhu00@gmail.com>
2020-09-22 12:21:05 -0400

    Added Lab7 documentation and code to repository

---
+++ Lab07/Lab7Part1.java
@@ -0,0 +1,7 @@
+public class Lab7Part1 {
+	public static void main(String[] args) {
+		System.out.println("Random Upper Case Generator:\n" + ((char)((int)65 + Math.random()*26)));
+				
+	}
+
+}
---
+++ Lab07/Lab7Part2.java
@@ -0,0 +1,19 @@
+public class Lab7Part2 {
+	public static void main(String[] args) {
+		System.out.println(""
+		+ ((char)((int)65 + Math.random()*26))
+		+ ((char)((int)65 + Math.random()*26))
+		+ ((char)((int)65 + Math.random()*26))
+		+ (int)(Math.random()*10)
+		+ (int)(Math.random()*10)
+		+ (int)(Math.random()*10)
+		+ (int)(Math.random()*10)
+		);
+		
+		
+		
+		
+		
+	}
+
+}
